f465221a233fb274348ededbe1e5c9962c341b5c
Goran Brbot <goran.brbot@mail.com>
2023-08-31 20:24:06 +0200

    initial commit

--- src/Aplikacija.js
+++ src/Aplikacija.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './windsock.svg';
 import './App.css';
 
 function App() {
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Moja prva aplikacija</p>
         <a
           className="App-link"
           href="https://reactjs.org"
